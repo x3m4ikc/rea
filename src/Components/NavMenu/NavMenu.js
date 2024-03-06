eedbceb279e03ef4366993e414f22e44a593014c
Вахитов Тимур <vt@greentechs.pro>
2024-03-06 18:13:47 +0300

    Setup humidity page

--- src/Components/NavMenu/NavMenu.js
+++ src/Components/NavMenu/NavMenu.js
@@ -7,47 +7,61 @@ const NavMenu = () => {
         <div className="nav-menu-holder">
             <ul className="navigation">
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but">
                         {/* <NavLink to='/' className={ ({ isActive }) => (isActive ? 'link-item active' : 'link-item')}>
                             <span className="nav=text">Главная</span>
                         </NavLink> */}
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Главная</span>
                         </a>
                     </button>
                 </li>
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but">
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Выбор программы</span>
                         </a>
                     </button>
                 </li>
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but">
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Управление</span>
                         </a>
                     </button>
                 </li>
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but">
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Температура</span>
                         </a>
                     </button>
                 </li>
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but active">
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Влажность и CO₂</span>
                         </a>
                     </button>
                 </li>
                 <li className="navigation-item">
-                    <button>
+                    <button className="nav-but">
                         <a>
-                            <span>Главная</span>
+                            <span className="nav-text">Растворный узел</span>
+                        </a>
+                    </button>
+                </li>
+                <li className="navigation-item">
+                    <button className="nav-but">
+                        <a>
+                            <span className="nav-text">Расписание поливов</span>
+                        </a>
+                    </button>
+                </li>
+                <li className="navigation-item">
+                    <button className="nav-but">
+                        <a>
+                            <span className="nav-text">Освещение</span>
                         </a>
                     </button>
                 </li>
--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -4,13 +4,16 @@ const TopBar = () => {
     return (
             <div className="buttons-holder-topbar">
                 <button className="button-topbar">
-                    Lalalala
+                    <span className="span-topbar">Датчик влажности 1</span>
+                    <p className="sensor-value">65% ()</p>
                 </button>
                 <button className="button-topbar">
-                    Lalalala
+                    <span className="span-topbar">Датчик влажности 2</span>
+                    <p className="sensor-value">67% ()</p>
                 </button>
                 <button className="button-topbar">
-                    Lalalala
+                    <span className="span-topbar">Датчик уровня CO₂</span>
+                    <p className="sensor-value">450 PPM ()</p>
                 </button>
             </div>
     )
