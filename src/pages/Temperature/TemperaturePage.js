3a2b065a562288fd9aef2994edc19d436e239819
Вахитов Тимур <vt@greentechs.pro>
2024-03-21 18:00:42 +0300

    Pointers on Temp Page

--- src/pages/Temperature/TemperaturePage.js
+++ src/pages/Temperature/TemperaturePage.js
@@ -1,5 +1,6 @@
 import NavMenu from "../../Components/NavMenu/NavMenu";
 import image from '../../static/images/room.png'
+import LensIcon from '@mui/icons-material/Lens';
 import './TemperaturePage.css'
 
 const TemperaturePage = () => {
@@ -59,8 +60,21 @@ const ImageHolder = () => {
     return (
         <div>
             <div className="pointer1"></div>
+            <LensIcon className="pointer_icon_1" sx={{ fontSize: 15 }}/>
             <div className="pointer2"></div>
+            <LensIcon className="pointer_icon_2" sx={{ fontSize: 15 }}/>
             <div className="pointer3"></div>
+            <LensIcon className="pointer_icon_3" sx={{ fontSize: 15 }}/>
+            <div className="pointer4"></div>
+            <LensIcon className="pointer_icon_4" sx={{ fontSize: 15 }}/>
+            <div className="pointer5"></div>
+            <LensIcon className="pointer_icon_5" sx={{ fontSize: 15 }}/>
+            <div className="pointer6"></div>
+            <LensIcon className="pointer_icon_6" sx={{ fontSize: 15 }}/>
+            <div className="pointer7"></div>
+            <LensIcon className="pointer_icon_7" sx={{ fontSize: 15 }}/>
+            <div className="pointer8"></div>
+            <LensIcon className="pointer_icon_8" sx={{ fontSize: 15 }}/>
         
             <div className="img-holder">
                 <img src={image} className="room_image"/>
